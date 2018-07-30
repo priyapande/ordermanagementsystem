b3ea8915809a98604fc08209e6110efce73ed197
priyapande <priyapande07@gmail.com>
2018-07-30 21:20:54 +0530

    order securities

--- js/get_securities.js
+++ js/get_securities.js
@@ -1,24 +1,24 @@
-function get_securities() {    
-    
+function get_securities() {
+
     var store=window.localStorage;
-    console.log("insside the function()");    
-    //Get the current date   
+    console.log("insside the function()");
+    //Get the current date
     var today = new Date();
     var dd = today.getDate();
-    var mm = today.getMonth()+1; 
+    var mm = today.getMonth()+1;
     var yyyy = today.getFullYear();
-    if(dd<10) 
+    if(dd<10)
     {
         dd='0'+dd;
-    } 
-    if(mm<10) 
+    }
+    if(mm<10)
     {
         mm='0'+mm;
     }
     today = mm+'-'+dd+'-'+yyyy;
     console.log(today);
     //Get the current time
-    var date = new Date(); 
+    var date = new Date();
     var hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
     var minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
     var curret_time = hours + ":" + minutes;
@@ -29,13 +29,13 @@ function get_securities() {
         };
 
     var xhttp = new XMLHttpRequest();
-    // var url = "http://192.168.43.34:7887/getAllSecurities/"+today+"/"+curret_time; 
-    var url = "http://192.168.43.34:7887/getAllSecurities/29-07-2018/20:30"; 
-        console.log(url);
-        xhttp.open("get", url, true);
+    // var url = "http://192.168.43.34:7887/getAllSecurities/"+today+"/"+curret_time;
+    var url = "http://192.168.43.34:7887/getAllSecurities/29-07-2018/20:30";
+    console.log(url);
+    xhttp.open("get", url, true);
         //xhttp.setRequestHeader('Content-Type','application/json');
-        xhttp.send();
-    
+    xhttp.send();
+
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
             // Typical action to be performed when the document is ready:
@@ -44,7 +44,6 @@ function get_securities() {
             // convert string to JSON
             //  var response = $.parseJSON(xhttp.responseText);
             var response = JSON.parse(xhttp.responseText);
-            delete response[""];
 
             //delete keys not required in JSON Object
             for(var k=0;k<response.length;k++)
@@ -52,22 +51,26 @@ function get_securities() {
                 delete response[k].buyCount;
                 delete response[k].sellCount;
             }
-            // EXTRACT VALUE FOR HTML HEADER. 
+
+            // EXTRACT VALUE FOR HTML HEADER.
             var col = [];
+            console.log(response);
             for (var i = 0; i < response.length; i++) {
                 for (var key in response[i]) {
+                  console.log(key);
                     if (col.indexOf(key) === -1) {
                         col.push(key);
                     }
                 }
             }
+            console.log(col);
 
-           
+
             // var fieldOrder = [""];
 
             // CREATE DYNAMIC TABLE.
             var table = document.createElement("table");
-            var tr = table.insertRow(-1); 
+            var tr = table.insertRow(-1);
 
             // for (var i = 0; i < col.length; i++) {
             //     var th = document.createElement("th");      // TABLE HEADER.
@@ -87,22 +90,21 @@ function get_securities() {
                     tabCell.innerHTML = response[i][col[j]];
                 }
 
-                tr.onclick = function() {
-                    var row=[];
-                    for(var k=0;k<7;k++)
-                    {
-                        row.push(tr.cells[k].innerHTML);
-                    }
-                  
-                    console.log(store);
-                    console.log(row);
-
-                    store.setItem("security_details",row);
-
-                    window.location="../templates/order_book.html"
-                }
-                
             }
+
+            // tr.onclick = function() {
+            //     var row=[];
+            //     for(var k=0;k<7;k++)
+            //     {
+            //         row.push(tr.cells[k].innerHTML);
+            //     }
+            //
+            //
+            //     console.log(row);
+            //
+            //     store.setItem("security_details",row);
+            //
+            // }
 
             // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
             var divContainer = document.getElementById("showData");
@@ -111,4 +113,30 @@ function get_securities() {
 
             }
         };
+}
+
+function addRowHandlers() {
+  var store=window.localStorage;
+  var table = document.getElementById("showData");
+  var rows = table.getElementsByTagName("tr");
+  console.log(rows);
+  var data=[];
+  for (i = 1; i < rows.length; i++) {
+    var currentRow = rows[i];
+    var createClickHandler = function(currentRow) {
+      return function() {
+        var cells = currentRow.getElementsByTagName("td");
+        for(var k=0;k<cells.length;k++){
+          var cell = cells[k];
+          var id = cell.innerHTML;
+          data.push(id);
+        //  alert("id:" + id);
+        }
+        store.setItem("security_details",data);
+        window.location="../templates/order_book.html"
+        console.log(store);
+      };
+    };
+    currentRow.onclick = createClickHandler(currentRow);
+  }
 }
--- js/order_book.js
+++ js/order_book.js
@@ -4,11 +4,11 @@ function order_book_mgmt()
     var store=window.localStorage;
     var security= store.security_details.split(',');
     document.getElementById("header_title").innerHTML="Current top prices for " + security[0];
-
+    console.log(store);
 
         var xhttp = new XMLHttpRequest();
-        var url = "http://192.168.43.34:7887/getTopOrders/"+security[2]; 
-        console.log(url);
+        var url = "http://192.168.43.34:7887/getTopOrders/"+security[2];
+        //console.log(url);
         xhttp.open("get", url, true);
         //xhttp.setRequestHeader('Content-Type','application/json');
         xhttp.send();
@@ -19,51 +19,36 @@ function order_book_mgmt()
 
             // convert string to JSON
             var response = JSON.parse(xhttp.responseText);
-            var buy = response.buy;
-            var sell = response.sell;
-            console.log(sell);
 
-            // EXTRACT VALUE FOR HTML HEADER. 
-            var col = [];
-            for (var i = 0; i < response.length; i++) {
-                for (var key in response[i]) {
-                    if (col.indexOf(key) === -1) {
-                        col.push(key);
-                    }
-                }
-            }
+            console.log(response);
 
-        //     // var fieldOrder = [""];
 
             // CREATE DYNAMIC TABLE.
             var table = document.createElement("table");
-            var tr = table.insertRow(-1); 
+            var tr = table.insertRow(-1);
 
             // ADD JSON DATA TO THE TABLE AS ROWS.
-            for (var i = 0; i < buy.length; i++) {
+            for (var i = 0; i < response[0].length; i++) {
                 tr = table.insertRow(-1);
-                var tabCell = tr.insertCell(-1);
-                var j1 = 1;
-                tabCell.classList.add("cell100")
-                tabCell.classList.add("column" + j1)
-                        tabCell.innerHTML = buy[i][col[0]];
-        }
 
-        for (var i = 0; i < sell.length; i++) {
-            tr = table.insertRow(-1);
-            var tabCell = tr.insertCell(-1);
-            var j1 = 2;
-            tabCell.classList.add("cell100")
-            tabCell.classList.add("column" + j1)
-                    tabCell.innerHTML = buy[i][col[1]];
-    }
+                for (var j = 0; j < response.length; j++) {
+                    var tabCell = tr.insertCell(-1);
+                    var j1 = j + 1;
+                    tabCell.classList.add("cell100")
+                    tabCell.classList.add("column" + j1)
+                    var t = response[j][i];
+                    tabCell.innerHTML = t.price;
+                }
+
+        }
+        //console.log(table);
 
             // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
-            var divContainer = document.getElementById("showData");
+            var divContainer = document.getElementById("sData");
             divContainer.innerHTML = "";
             divContainer.appendChild(table);
-
+            console.log(divContainer);
            }
         };
-    
+
 }
