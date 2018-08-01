223c41d184a2626295aa8b95b1987631a7816c8a
aakash_goyal <aakashgoyal@pnplabs.in>
2018-08-01 17:14:44 +0530

    minor changes

--- js/view_accounts.js
+++ js/view_accounts.js
@@ -2,52 +2,62 @@ function calculate_receivables()
 {
 
     var store=window.localStorage;
-    var client_code= store.client_code;
-    //console.log(store);
+    var client_code= store.getItem("client_code");
+    console.log(store);
 
-         var xhttp = new XMLHttpRequest();
-         var url = "http://192.168.43.34:7887/getNetPayableReceivable/"+client_code;
-         //console.log(url);
-         xhttp.open("get", url, true);
-        //xhttp.setRequestHeader('Content-Type','application/json');
-         xhttp.send();
-         xhttp.onreadystatechange = function() {
-             if (this.readyState == 4 && this.status == 200) {
-             // Typical action to be performed when the document is ready:
-             console.log(xhttp.responseText);
+        var xhttp = new XMLHttpRequest();
+        var url = "http://192.168.43.34:7887/getReportByClient/"+client_code;
+        console.log(url);
+        xhttp.open("get", url, true);
+        xhttp.setRequestHeader('Content-Type','application/json');
+        xhttp.send();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+            // Typical action to be performed when the document is ready:
+            console.log(xhttp.responseText);
 
-             // convert string to JSON
-             var response = JSON.parse(xhttp.responseText);
+            // convert string to JSON
+            var response = JSON.parse(xhttp.responseText);
+            console.log(response);
 
-        //     console.log(response);
+               // EXTRACT VALUE FOR HTML HEADER.
+               var col = [];
+               console.log(response);
+               for (var i = 0; i < response.length; i++) {
+                   for (var key in response[i]) {
+                     console.log(key);
+                       if (col.indexOf(key) === -1) {
+                           col.push(key);
+                       }
+                   }
+               }
+               console.log(col);
+   
+     // CREATE DYNAMIC TABLE.
+     var table = document.createElement("table");
+     var tr = table.insertRow(-1);
 
 
-        //     // CREATE DYNAMIC TABLE.
-        //     var table = document.createElement("table");
-        //     var tr = table.insertRow(-1);
+     // ADD JSON DATA TO THE TABLE AS ROWS.
+     for (var i = 0; i < response.length; i++) {
+         tr = table.insertRow(-1);
 
-        //     // ADD JSON DATA TO THE TABLE AS ROWS.
-        //     for (var i = 0; i < response[0].length; i++) {
-        //         tr = table.insertRow(-1);
+         for (var j = 0; j < col.length; j++) {
+             var tabCell = tr.insertCell(-1);
+             var j1 = j + 1;
+             tabCell.classList.add("cell100")
+             tabCell.classList.add("column" + j1)
+             tabCell.innerHTML = response[i][col[j]];
+         }
 
-        //         for (var j = 0; j < response.length; j++) {
-        //             var tabCell = tr.insertCell(-1);
-        //             var j1 = j + 1;
-        //             tabCell.classList.add("cell100")
-        //             tabCell.classList.add("column" + j1)
-        //             var t = response[j][i];
-        //             tabCell.innerHTML = t.price;
-        //         }
+     }
 
-        // }
-        // //console.log(table);
 
-        //     // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
-        //     var divContainer = document.getElementById("sData");
-        //     divContainer.innerHTML = "";
-        //     divContainer.appendChild(table);
-        //     console.log(divContainer);
-        //    }
-        // };
+     // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+     var divContainer = document.getElementById("sData");
+     divContainer.innerHTML = "";
+     divContainer.appendChild(table);
+   }
+        };
 
 }
